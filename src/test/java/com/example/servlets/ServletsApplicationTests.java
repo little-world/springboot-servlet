492cad87350790500bbe15b9d898530e71f9b4d1
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2021-01-04 13:42:55 +0100

    first

---
+++ src/test/java/com/example/servlets/ServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
